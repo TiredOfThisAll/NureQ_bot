ff11aab3c70f13745367ad18f61b705c9543283d
TiredOfThisAll <vrublevskiy.2015@gmail.com>
2020-12-26 22:13:05 +0300

    Implemented functions to interact with db

--- repository.py
+++ repository.py
@@ -4,11 +4,29 @@ class Repository:
         self.cursor = connection.cursor()
 
     def create_schema(self):
-        pass
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS queues (
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL UNIQUE
+            )
+        """)
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS pupils (
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL UNIQUE,
+                queue_id INTEGER NOT NULL,
+                FOREIGN KEY (queue_id)
+                    REFERENCES queues (id)
+                        ON DELETE CASCADE
+                        ON UPDATE NO ACTION
+            )
+        """)
 
     def create_queue(self, queue_name):
-        pass
+        self.cursor.execute("""
+            INSERT INTO queues (name)
+            VALUES (?)
+        """, (queue_name,))
 
     def commit(self):
-        pass
-
+        self.connection.commit()
