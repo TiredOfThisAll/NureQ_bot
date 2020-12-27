651f0e630a184e0aec7ed15c08fe5761d1bb6a0e
levant <levant.ba@gmail.com>
2020-12-27 22:06:18 +0200

    Create a stub implementation of 'Controller'

---
+++ controller.py
@@ -0,0 +1,12 @@
+class Controller:
+    def __init__(self):
+        pass
+
+    def prompt_queue_name(self):
+        pass
+
+    def respond_to_prompted_queue_name(self):
+        pass
+
+    def echo_message(self):
+        pass
