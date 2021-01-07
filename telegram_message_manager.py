90e1913074620f5a5f3ec4e9d528811e54b7c98d
levant <levant.ba@gmail.com>
2021-01-07 13:53:47 +0200

    Add support for 'reply_markup' to the 'TelegramMessageManager.send_message' function

--- telegram_message_manager.py
+++ telegram_message_manager.py
@@ -28,12 +28,15 @@ class TelegramMessageManager:
 
         return updates
 
-    def send_message(self, chat_id, response_text):
+    def send_message(self, chat_id, response_text, reply_markup=None):
+        query_parameters = {
+            "chat_id": chat_id,
+            "text": response_text,
+        }
+        if reply_markup is not None:
+            query_parameters["reply_markup"] = json.dumps(reply_markup)
         send_message_url = TELEGRAM_BOT_API_URL + self.token \
-            + "/sendMessage?" + urlencode({
-                "chat_id": chat_id,
-                "text": response_text
-            })
+            + "/sendMessage?" + urlencode(query_parameters)
         with urlopen(send_message_url):
             pass
 
