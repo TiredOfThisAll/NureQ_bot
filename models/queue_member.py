c990c769f898d1e7e531a4ffd543e949f57f2e1a
levant <levant.ba@gmail.com>
2021-02-09 19:24:54 +0200

    Update 'QueueMember' to match the new schema

--- models/queue_member.py
+++ models/queue_member.py
@@ -1,7 +1,19 @@
 class QueueMember:
-    def __init__(self, id, member_name, queue_id, crossed):
+    def __init__(
+        self,
+        id,
+        user_id,
+        user_first_name,
+        user_last_name,
+        user_username,
+        queue_id,
+        crossed
+    ):
         self.id = id
-        self.member_name = member_name
+        self.user_id = user_id
+        self.user_first_name = user_first_name
+        self.user_last_name = user_last_name
+        self.user_username = user_username
         self.queue_id = queue_id
         self.crossed = crossed
 
@@ -10,16 +22,29 @@ class QueueMember:
             queue_member_tuple[0],
             queue_member_tuple[1],
             queue_member_tuple[2],
-            queue_member_tuple[3]
+            queue_member_tuple[3],
+            queue_member_tuple[4],
+            queue_member_tuple[5],
+            queue_member_tuple[6],
         )
 
     def format_queue_string(self, queue_pos):
-        result = str(queue_pos) + ". @" + self.member_name + "\n"
+        formatted_name = self.user_first_name
+        if self.user_last_name is not None:
+            formatted_name += " " + self.user_last_name
+        formatted_name \
+            = f"<a href='tg://user?id={self.user_id}'>{formatted_name}</a>"
+        result = f"{queue_pos}. {formatted_name}\n"
         if self.crossed == 1:
-            return "<s>" + result + "</s>"
+            return f"<s>{result}</s>"
         return result
 
     def __str__(self):
-        return "QueueMember: id = " + str(self.id) + ", member_name = " \
-            + self.member_name + ", queue_id = " + str(self.queue_id) \
-            + ", crossed = " + str(self.crossed)
+        return "QueueMember: " \
+            + f"id = {self.id}, " \
+            + f"user_id = {self.user_id}, " \
+            + f"user_first_name = {self.user_first_name}, " \
+            + f"user_last_name = {self.user_last_name}, " \
+            + f"user_username = {self.user_username}, " \
+            + f"queue_id = {self.queue_id}, " \
+            + f"crossed = {self.crossed}"
