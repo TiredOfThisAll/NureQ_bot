c8f2179df363bccfef66dd61bfddb857300d07a6
xjLsdOaV <vrublevskiy.2015@gmail.com>
2021-03-06 15:24:16 +0300

    formatting datetime from last_updated_on

--- src/data_access/models/queue.py
+++ src/data_access/models/queue.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class Queue:
     def __init__(self, id, name, last_updated_on):
         self.id = id
@@ -6,6 +8,10 @@ class Queue:
 
     def from_tuple(queue_tuple):
         return Queue(queue_tuple[0], queue_tuple[1], queue_tuple[2])
+
+    def get_formatted_last_updated_on(self):
+        return datetime.fromisoformat(self.last_updated_on) \
+            .strftime("%d.%m.%Y %H:%M")
 
     def __str__(self):
         return "Queue: " \
