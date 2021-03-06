317bb93e47da2e7691b1c37e1ea2eaa55249abd0
levant <levant.ba@gmail.com>
2021-03-06 12:26:39 +0200

    Add basic template for the queues page

--- src/web_main.py
+++ src/web_main.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from os import path
 
-app = Flask(__name__)
+PROJECT_PATH = path.abspath(path.join(__file__, "..", ".."))
+
+app = Flask(
+    __name__,
+    template_folder=path.join(PROJECT_PATH, "src", "web", "templates")
+)
 
 @app.route("/")
 def queues():
-    return "Queues Page"
+    return render_template("queues.html")
 
 app.run()
