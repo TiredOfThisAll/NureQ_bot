6a369d16c451629c5da19ae1c6107150ebc4e03c
xjLsdOaV <vrublevskiy.2015@gmail.com>
2021-03-13 13:15:01 +0300

    Asking the user to confirm the deletion and then delete

--- src/web/static/index.js
+++ src/web/static/index.js
@@ -1,3 +1,13 @@
 const deleteQueue = (queueId) => {
-    confirm(`Чел, а ты уверен вообще? Ты вообще-то хочешь удалить очередь с идишником ${queueId}`);
+    let isDelete = confirm(`Чел, а ты уверен вообще? Ты вообще-то хочешь удалить очередь с идишником ${queueId}`);
+    if (!isDelete) {
+        return;
+    }
+    fetch(`/queues/${queueId}`, {method: "DELETE"})
+        .then(response => {
+            if (response.status !== 201) {
+                return Promise.reject();
+            }
+            location.reload();
+        });
 };
