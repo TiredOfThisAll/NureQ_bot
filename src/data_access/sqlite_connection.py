e0842d21a5a59680ebf6526a7ddb19262dc2b013
xjLsdOaV <vrublevskiy.2015@gmail.com>
2021-03-20 11:55:23 +0200

    Removed extra cursor

--- src/data_access/sqlite_connection.py
+++ src/data_access/sqlite_connection.py
@@ -3,5 +3,5 @@ import sqlite3
 
 def create_sqlite_connection(database_path):
     connection = sqlite3.connect(database_path)
-    connection.cursor().execute("PRAGMA foreign_keys = ON")
+    connection.execute("PRAGMA foreign_keys = ON")
     return connection
