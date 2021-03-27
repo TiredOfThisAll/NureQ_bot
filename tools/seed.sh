bed5168d0e4a88495ba0d739bd7cbdc4c168a2ed
xjLsdOaV <vrublevskiy.2015@gmail.com>
2021-03-27 12:46:13 +0200

    seed sh

--- tools/seed.sh
+++ tools/seed.sh
@@ -15,7 +15,10 @@ echo "
   INSERT INTO queue_members (user_id, user_first_name, user_last_name, user_username, queue_id, crossed)
   VALUES
     (1, 'Anton', 'Bodiak', 'levant47', 1, 0),
-    (2, 'Sanya', 'Zlobin', 'Zhaba', 2, 1),
-    (3, 'Anton', 'Bodiak', 'levant47', 3, 1);
+    (2, 'Sanya', 'Sanya', 'Sanya', 1, 1),
+    (3, 'Danya', 'Danya', 'Danya', 1, 0),
+    (4, 'Anthony', 'Anthony', 'Anthony', 1, 1),
+    (5, 'Sanya', 'Zlobin', 'Zhaba', 2, 1),
+    (6, 'Anton', 'Bodiak', 'levant47', 3, 1);
 " | sqlite3 ../nureq.db
 
