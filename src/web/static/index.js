9be792b17e11f4bb7d1e33ca118d333ffa1f60db
xjLsdOaV <vrublevskiy.2015@gmail.com>
2021-03-27 11:57:54 +0200

    deleteMember function

--- src/web/static/index.js
+++ src/web/static/index.js
@@ -10,6 +10,27 @@ const deleteQueue = queueId => {
                 return Promise.reject();
             }
             location.reload();
+        })
+        .finally(() => {
+            setSpinnerVisibility(false)
+        });
+};
+
+const deleteMember = (queueId, queueName, queueMemberName, queueMemberUserId) => {
+    const isConfirmed = confirm(`Вы уверены, что хотите удалить ${queueMemberName} из ${queueName} очереди`);
+    if (!isConfirmed) {
+        return;
+    }
+    setSpinnerVisibility(true);
+    fetch(`/api/queues/${queueId}/members/${queueMemberUserId}`, {method: "DELETE"})
+        .then(response => {
+            if (response.status !== 204) {
+                return Promise.reject();
+            }
+            location.reload();
+        })
+        .finally(() => {
+            setSpinnerVisibility(false)
         });
 };
 
