16a8c55de833771fe979f0520ac609dd5eaba3f9
xjLsdOaV <vrublevskiy.2015@gmail.com>
2021-03-28 15:09:35 +0300

    Update seed sh to seed the position

--- tools/seed.sh
+++ tools/seed.sh
@@ -1,8 +1,7 @@
 echo "
   PRAGMA foreign_keys = ON;
 
-  DELETE
-  FROM queues;
+  DELETE FROM queues;
 
   INSERT INTO queues (name, last_updated_on)
   VALUES
@@ -12,13 +11,13 @@ echo "
     ('fourth', date('now')),
     ('fifth', date('now'));
 
-  INSERT INTO queue_members (user_id, user_first_name, user_last_name, user_username, queue_id, crossed)
+  INSERT INTO queue_members (user_id, user_first_name, user_last_name, user_username, queue_id, crossed, position)
   VALUES
-    (1, 'Anton', 'Bodiak', 'levant47', 1, 0),
-    (2, 'Sanya', 'Sanya', 'Sanya', 1, 1),
-    (3, 'Danya', 'Danya', 'Danya', 1, 0),
-    (4, 'Anthony', 'Anthony', 'Anthony', 1, 1),
-    (5, 'Sanya', 'Zlobin', 'Zhaba', 2, 1),
-    (6, 'Anton', 'Bodiak', 'levant47', 3, 1);
+    (1, 'Anton', 'Bodiak', 'levant47', 1, 1, 0),
+    (2, 'Sanya', 'Sanya', 'Sanya', 1, 1, 2),
+    (3, 'Danya', 'Danya', 'Danya', 1, 1, 1),
+    (4, 'Anthony', 'Anthony', 'Anthony', 1, 0, 3),
+    (5, 'Sanya', 'Zlobin', 'Zhaba', 2, 0, 0),
+    (6, 'Anton', 'Bodiak', 'levant47', 3, 0, 0);
 " | sqlite3 ../nureq.db
 
