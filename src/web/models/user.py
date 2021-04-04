3a61e2e778b19363e0d99e848935e8a7eda5286a
levant47 <levant.ba@gmail.com>
2021-04-04 13:22:12 +0300

    Create 'User' web model

---
+++ src/web/models/user.py
@@ -0,0 +1,15 @@
+class User:
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.user_id)
