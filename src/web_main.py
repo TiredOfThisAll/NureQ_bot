f647192fa39c01c9c0735f89c5d1a8fe13dde0c6
levant47 <levant.ba@gmail.com>
2021-04-10 11:21:44 +0300

    Revert "check if the user is an admin"
    This reverts commit bebe581d851c33ca28703a87de77b26a7c579e47.

--- src/web_main.py
+++ src/web_main.py
@@ -105,9 +105,6 @@ def telegram_login_successful():
     # и у нас тут будет в query parameter-ах приходить объект с полями id,
     # hash, auth_date
     user = User(request.args["id"])
-    admins = context.repository.get_admins_id()
-    if int(user.user_id) not in admins:
-        return redirect(url_for("login"))
     login_user(user)
 
     # 1. спарсить query parameter-ы (там будет user ID, username и т.д.)
@@ -182,4 +179,4 @@ def pull_down_queue_member(queue_id, action):
     return "", 204
 
 
-app.run(port=80)
+app.run()
