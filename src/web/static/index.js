c274bdf5c412c737353ed77dfd44896529bf8027
xjLsdOaV <vrublevskiy.2015@gmail.com>
2021-12-07 18:05:10 +0200

    header for mobile phones

--- src/web/static/index.js
+++ src/web/static/index.js
@@ -1,3 +1,12 @@
 const setSpinnerVisibility = isVisible => {
     document.getElementById("spinner").style.visibility = isVisible ? "visible" : "hidden";
 };
+
+const handleBurgerMenu = () => {
+    const nav = document.querySelector("header > nav");
+    if (nav.style.display === "") {
+        nav.style.display = "block";
+    } else {
+        nav.style.display = "";
+    }
+};
