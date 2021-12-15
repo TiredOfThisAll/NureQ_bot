0ceb1a28575f444a1e505bff549127f0a8ac7bf8
levant47 <levant.ba@gmail.com>
2021-12-15 16:38:47 +0200

    This still needs to be refactored

--- src/web/static/edit_queue.js
+++ src/web/static/edit_queue.js
@@ -141,17 +141,23 @@ const handleQueueMemberRowDrop = event => {
     const draggedQueueMemberRow = document.getElementById(dragState.draggedQueueMemberRowId);
     const dropTargetQueueMemberRow = event.currentTarget;
 
+    // clear highlighting
+    dropTargetQueueMemberRow.classList.remove("drop-to-lower-half", "drop-to-upper-half");
+
+    dropQueueMemberRow(event.clientY, draggedQueueMemberRow, dropTargetQueueMemberRow);
+};
+
+const dropQueueMemberRow = (cursorPositionY, draggedQueueMemberRow, dropTargetQueueMemberRow) => {
     let insertedBefore;
-    if (isInUpperHalf(event.clientY, dropTargetQueueMemberRow)) {
+    if (isInUpperHalf(cursorPositionY, dropTargetQueueMemberRow)) {
         insertBefore(draggedQueueMemberRow, dropTargetQueueMemberRow);
         insertedBefore = true;
-    } else if (isInLowerHalf(event.clientY, dropTargetQueueMemberRow)) {
+    } else if (isInLowerHalf(cursorPositionY, dropTargetQueueMemberRow)) {
         insertAfter(draggedQueueMemberRow, dropTargetQueueMemberRow);
         insertedBefore = false;
     } else {
         return;
     }
-    dropTargetQueueMemberRow.classList.remove("drop-to-lower-half", "drop-to-upper-half");
     renderQueuePositions();
 
     const queueId = dropTargetQueueMemberRow.dataset.queueId;
@@ -170,6 +176,104 @@ const handleQueueMemberRowDrop = event => {
                 return Promise.reject();
             }
         });
+};
+
+let dragThumbnailElement = null;
+let prevHighlightedRow = null;
+let mobileDraggedElement = null;
+let currentTouchId = null;
+
+const updateDragThumbnailElementPosition = cursorPosition => {
+    // make sure to use document coordinates instead of screen coordinates
+    dragThumbnailElement.style.top = `${cursorPosition.y}px`;
+    // TODO: comment about Math.max
+    dragThumbnailElement.style.right = `${Math.max(0, window.innerWidth - cursorPosition.x)}px`;
+};
+
+const handleQueueMemberRowTouchstart = event => {
+    // do not handle multiple touches at once
+    if (currentTouchId !== null && event.changedTouches[0].identifier !== currentTouchId) {
+        return;
+    }
+    currentTouchId = event.changedTouches[0].identifier;
+    mobileDraggedElement = event.currentTarget.parentElement;
+    // create drag thumbnail
+    dragThumbnailElement = document.createElement("span");
+    dragThumbnailElement.textContent = mobileDraggedElement.children[1].textContent.trim();
+    dragThumbnailElement.style.userSelect = "none";
+    dragThumbnailElement.style.position = "absolute";
+    globalCursorPagePosition.x = event.changedTouches[0].pageX;
+    globalCursorPagePosition.y = event.changedTouches[0].pageY;
+    globalCursorPosition.x = event.changedTouches[0].clientX;
+    globalCursorPosition.y = event.changedTouches[0].clientY;
+    updateDragThumbnailElementPosition(globalCursorPagePosition);
+    document.body.prepend(dragThumbnailElement);
+};
+
+const TOUCH_SCROLL_SPEED = window.innerHeight * 0.01;
+const TOUCH_SCROLL_SENSITIVITY_RANGE = 50;
+
+const isCloseToScreenEdge = cursorPosition => {
+    if (cursorPosition.y < TOUCH_SCROLL_SENSITIVITY_RANGE) {
+        return "close_to_top_edge";
+    }
+    if (window.innerHeight - cursorPosition.y < TOUCH_SCROLL_SENSITIVITY_RANGE) {
+        return "close_to_bottom_edge";
+    }
+    return "not_close";
+};
+
+const handleQueueMemberRowTouchmove = event => {
+    event.preventDefault(); // prevent the browser from reacting to gestures, while the user is drag-n-dropping
+
+    // do not handle multiple touches at once
+    if (currentTouchId !== null && event.changedTouches[0].identifier !== currentTouchId) {
+        return;
+    }
+};
+
+const handleQueueMemberRowTouchend = event => {
+    // do not handle multiple touches at once
+    if (currentTouchId !== null && event.changedTouches[0].identifier !== currentTouchId) {
+        return;
+    }
+    currentTouchId = null;
+    // clear previous highlight
+    if (prevHighlightedRow !== null) {
+        prevHighlightedRow.classList.remove("drop-to-upper-half", "drop-to-lower-half");
+    }
+
+    // remove drag thumbnail
+    dragThumbnailElement.parentElement.removeChild(dragThumbnailElement);
+    dragThumbnailElement = null;
+
+    const draggedQueueMemberRow = event.currentTarget.parentElement;
+    const cursorPosition = getCursorPositionForTouchEvent(event);
+    const dropTargetQueueMemberRow = getQueueMemberRowUnderCursor(cursorPosition);
+    // do nothing if dropping on the same row or not a row element at all
+    if (dropTargetQueueMemberRow === null || draggedQueueMemberRow === dropTargetQueueMemberRow) {
+        return;
+    }
+
+    dropQueueMemberRow(cursorPosition.y, draggedQueueMemberRow, dropTargetQueueMemberRow);
+};
+
+const getCursorPositionForTouchEvent = event => { // in screen coordinates
+    return {x: event.changedTouches[0].clientX, y: event.changedTouches[0].clientY};
+};
+
+const getQueueMemberRowUnderCursor = cursorPosition => {
+    const hits = document.elementsFromPoint(cursorPosition.x, cursorPosition.y);
+    for (const hit of hits) {
+        let result = hit;
+        while (result !== null) {
+            if (result.id.startsWith("queue-member-")) {
+                return result;
+            }
+            result = result.parentElement;
+        }
+    }
+    return null;
 };
 
 const renderQueuePositions = () => {
@@ -202,10 +306,76 @@ const isInLowerHalf = (positionY, element) => {
     return isBetween(elementMidPoint, positionY, elementRect.bottom);
 };
 
+const globalCursorPosition = {x: 0, y: 0};
+const globalCursorPagePosition = {x: 0, y: 0};
+
+const trackCursorPosition = touchMoveEvent => {
+    if (currentTouchId !== null && event.changedTouches[0].identifier !== currentTouchId) {
+        return;
+    }
+    const p = getCursorPositionForTouchEvent(touchMoveEvent);
+    globalCursorPosition.x = p.x;
+    globalCursorPosition.y = p.y;
+    globalCursorPagePosition.x = touchMoveEvent.changedTouches[0].pageX;
+    globalCursorPagePosition.y = touchMoveEvent.changedTouches[0].pageY;
+};
+
+const scrollAtEdgesWhenDragNDroppingOnMobile = () => {
+    if (dragThumbnailElement !== null) {
+        const cursorPosition = globalCursorPosition;
+        const proximity = isCloseToScreenEdge(cursorPosition);
+        switch (proximity) {
+            case "close_to_top_edge":
+                window.scrollBy(0, -TOUCH_SCROLL_SPEED);
+                break;
+            case "close_to_bottom_edge":
+                if (globalCursorPagePosition.y < document.body.parentElement.offsetHeight - 50) {
+                    window.scrollBy(0, TOUCH_SCROLL_SPEED);
+                }
+                break;
+            case "not_close":
+                // do nothing
+                break;
+        }
+
+        // move drag thumbnail
+        updateDragThumbnailElementPosition(globalCursorPagePosition);
+
+        // clear previous highlight
+        if (prevHighlightedRow !== null) {
+            prevHighlightedRow.classList.remove("drop-to-upper-half", "drop-to-lower-half");
+        }
+
+        // highlight drop area
+        const dropTargetQueueMemberRow = getQueueMemberRowUnderCursor(cursorPosition);
+        const sourceQueueMemberRow = mobileDraggedElement;
+        // skip highlighting if hovering over something other than a queue member row or over the same row
+        if (dropTargetQueueMemberRow !== null && dropTargetQueueMemberRow !== sourceQueueMemberRow) {
+            if (isInUpperHalf(cursorPosition.y, dropTargetQueueMemberRow)) {
+                dropTargetQueueMemberRow.classList.add("drop-to-upper-half");
+                prevHighlightedRow = dropTargetQueueMemberRow;
+            } else if (isInLowerHalf(cursorPosition.y, dropTargetQueueMemberRow)) {
+                dropTargetQueueMemberRow.classList.add("drop-to-lower-half");
+                prevHighlightedRow = dropTargetQueueMemberRow;
+            }
+        }
+    }
+
+    setTimeout(scrollAtEdgesWhenDragNDroppingOnMobile, 15);
+};
+
 (() => {
     document.getElementById("new_queue_name").addEventListener("keydown", event => {
         if (event.key === "Enter") {
             document.getElementById("save_queue_name_button").click();
         }
     });
+
+    document.addEventListener("scroll", () => {
+        globalCursorPagePosition.y = window.scrollY + globalCursorPosition.y;
+    });
+    document.addEventListener("touchmove", trackCursorPosition);
+
+    document.documentElement.style.scrollBehavior = 'auto';
+    scrollAtEdgesWhenDragNDroppingOnMobile();
 })();
