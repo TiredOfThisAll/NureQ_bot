fc9e0e3b41a982fe877053d73ce183b02735c5ea
levant47 <levant.ba@gmail.com>
2021-12-19 23:08:36 +0200

    Send messages about invalid commands only in private chats

--- src/bot/server/models/update_context.py
+++ src/bot/server/models/update_context.py
@@ -13,6 +13,9 @@ class UpdateContext:
     def from_update(update):
         update_context = UpdateContext()
         update_context.update = update
+        update_context.chat_type = update_context.update \
+            .get("chat", {}) \
+            .get("type")
 
         # if has text and is NOT a reply
         if update.get("message", {}).get("text") is not None \
